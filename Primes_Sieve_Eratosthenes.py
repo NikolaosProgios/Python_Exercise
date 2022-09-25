efca5914b30eeb28896f2b6d23d2e9dd95c1ad73
Nick <45072262+NikolaosProgios@users.noreply.github.com>
2022-09-25 12:18:49 +0300

    !

---
+++ Primes_Sieve_Eratosthenes.py
@@ -0,0 +1,12 @@
+﻿def primes_sieve(limit):
+	limitn=limit + 1
+	not_prime=set()
+	primes=[]
+	for i in range(2,limitn):
+	    if i in not_prime:
+	        continue
+	    for f in range(2*1,limitn,i):
+	        not_prime.add(f)
+	    primes.append(i)
+	return primes
+print primes_sieve(1000)
